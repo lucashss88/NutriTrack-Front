6a6926129f027e743156fa2c54d2d26d5358e8ec
lucashss88 <lucashss88@gmail.com>
2024-10-01 14:16:49 -0300

    editDiet patient || nutricionist

--- src/editDietPatient.js
+++ src/editDietPatient.js
@@ -5,17 +5,19 @@ import { useParams } from 'react-router-dom';
 import BackButton from './components/backbutton';
 
 const EditDietPatient = () => {
-  const { id: patientId } = useParams(); // Obtém o ID do paciente a partir dos parâmetros da URL
+  const { id: patientId } = useParams();
   const [diet, setDiet] = useState(null);
   const [foods, setFoods] = useState([]);
+  const [selectedFoodGroup, setSelectedFoodGroup] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const API_URL = process.env.REACT_APP_API_URL
+  const API_URL = process.env.REACT_APP_API_URL;
 
   const loadToken = () => {
     return localStorage.getItem('token');
   };
 
+  // Carrega a dieta do paciente
   useEffect(() => {
     const fetchDiet = async () => {
       try {
@@ -37,6 +39,7 @@ const EditDietPatient = () => {
     fetchDiet();
   }, [patientId]);
 
+  // Carrega os alimentos por grupo de alimento
   const handleFoodGroupChange = async (foodGroup) => {
     try {
       const token = loadToken();
@@ -45,32 +48,38 @@ const EditDietPatient = () => {
           'x-auth-token': token
         }
       });
-      setFoods(response.data);
+      setFoods(response.data.foods);
     } catch (error) {
       console.error('Error fetching foods by group:', error);
       setError('Error fetching foods by group');
     }
   };
 
+  // Atualiza o alimento da refeição
   const handleFoodChange = async (mealFoodId, newFoodId, foodGroup) => {
     try {
       const token = loadToken();
-      await axios.put(`${API_URL}/api/diets/update-food`, {
+      console.log(API_URL);
+      const updateResponse = await axios.put(`${API_URL}/api/diets/update-food`, {
         mealFoodId,
-        newFoodId
+        newFoodId,
+        foodGroup
       }, {
         headers: {
           'x-auth-token': token
         }
       });
 
-      // Atualiza a dieta após a mudança bem-sucedida
-      const response = await axios.get(`${API_URL}/api/diets/${patientId}`, {
+      console.log('Alimento atualizado:', updateResponse.data);
+
+      // Recarrega a dieta após atualização
+      const dietResponse = await axios.get(`${API_URL}/api/diets/${patientId}`, {
         headers: {
           'x-auth-token': token
         }
       });
-      setDiet(response.data);
+
+      setDiet(dietResponse.data);
 
       // Atualiza os alimentos do grupo
       await handleFoodGroupChange(foodGroup);
@@ -82,7 +91,6 @@ const EditDietPatient = () => {
     }
   };
 
-
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>{error}</p>;
   if (!diet) return <p>Nenhuma dieta encontrada.</p>;
@@ -91,47 +99,46 @@ const EditDietPatient = () => {
       <div>
         <BackButton />
         <div className="editdiet-container">
-          <div className="subblock">
-            <div className="title">
-              <h1>Sua Dieta</h1>
-            </div>
-            <div className="editdiet">
-              <div className='block-form label-editdiet'>
-                <label>Refeições:</label>
-                {diet.Meals && diet.Meals.length > 0 ? (
-                    diet.Meals.map(meal => (
-                        <span key={meal.id}>
-                          <h3>{meal.type}</h3>
-                          {meal.Food && meal.Food.length > 0 ? meal.Food.map(mealFood => (
-                              <div key={mealFood.id}>
-                                <span>{mealFood.name} - {mealFood.MealFood.quantity}g</span>
-                                <select
-                                    value={mealFood.Food ? mealFood.Food.id : ''}
-                                    onFocus={() => {
-                                      handleFoodGroupChange(mealFood.Food ? mealFood.Food.foodGroup : '');
-                                    }}
-                                    onChange={(e) => {
-                                      handleFoodChange(mealFood.id, e.target.value, mealFood.Food ? mealFood.Food.foodGroup : '');
-                                    }}
-                                >
-                                  <option value="">Selecione um alimento</option>
-                                  {foods.length > 0 && foods.map(food => (
-                                      <option key={food.id} value={food.id}>
-                                        {food.name}
-                                      </option>
-                                  ))}
-                                </select>
-                              </div>
-                          )) : 'Nenhuma comida encontrada'}
-                        </span>
-                    ))
-                ) : 'Nenhuma refeição encontrada'}
-              </div>
-            </div>
-          </div>
+          <h1>Editar Dieta</h1>
+          <form>
+            {diet.Meals && diet.Meals.length > 0 ? (
+                diet.Meals.map(meal => (
+                    <div key={meal.id} className="meal-section">
+                      <h3>{meal.type}</h3>
+                      {meal.Food && meal.Food.length > 0 ? meal.Food.map(mealFood => (
+                          <div key={mealFood.id} className="meal-food-item">
+                            <label>
+                              {mealFood.name} - {mealFood.MealFood.quantity}g
+                            </label>
+                            <select
+                                value={mealFood.Food ? mealFood.Food.id : ''}
+                                onFocus={() => {
+                                  handleFoodGroupChange(mealFood.Food ? mealFood.Food.foodGroup : '');
+                                  setSelectedFoodGroup(mealFood.Food ? mealFood.Food.foodGroup : '');
+                                }}
+                                onChange={(e) => {
+                                  handleFoodChange(mealFood.id, e.target.value, selectedFoodGroup);
+                                }}
+                            >
+                              <option value="">Selecione um alimento</option>
+                              {foods.length > 0 && foods.map(food => (
+                                  <option key={food.id} value={food.id}>
+                                    {food.name}
+                                  </option>
+                              ))}
+                            </select>
+                          </div>
+                      )) : 'Nenhuma comida encontrada'}
+                    </div>
+                ))
+            ) : (
+                <p>Nenhuma refeição encontrada.</p>
+            )}
+          </form>
         </div>
       </div>
   );
 };
 
 export default EditDietPatient;
+
